6e4c96d16491f353954a5fddf18bc0daf529dbef
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-07-06 02:35:01 +0300

    workshops
    basic python workshops

---
+++ workshops.py
@@ -0,0 +1,12 @@
+x = 10
+y = 20
+
+temp = 0
+
+temp = x
+x = y
+y = temp
+
+print("x =" +str(x))
+print("y =" +str(y))
+
---
+++ workshops1.py
@@ -0,0 +1,6 @@
+ConversionRate = 0.621
+km = int(input("How many km?"))
+
+mil = km * ConversionRate
+print(str(km) +"Km  = "+str(mil)+" mil ")
+
